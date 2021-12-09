0cb5b120e285896cedbb5dd2266d0379971b52c7
Cat <epay@ss8o.com>
2021-12-09 21:53:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author Vz_Cat
+ * Created with IntelliJ IDEA.
+ * Created on 2021-12-09 21:50
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
